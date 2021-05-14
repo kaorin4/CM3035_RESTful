36664e3438df797579477d9bd4370036df8bbeb2
Kaori Murakami <kaori.murakami4@gmail.com>
2021-05-13 19:32:30 -0500

    changed project settings

--- bioproject/bioproject/settings.py
+++ bioproject/bioproject/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -25,7 +28,12 @@ SECRET_KEY = 'django-insecure-k)ff*6w-pdc#!_m5*4ga=sp8vkcb(+dt0xg@=oz(1k5fe40&kw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.coursera-apps.org',
+    'udrrctlw.labs.coursera.org',
+]
 
 
 # Application definition
@@ -77,7 +85,8 @@ WSGI_APPLICATION = 'bioproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
